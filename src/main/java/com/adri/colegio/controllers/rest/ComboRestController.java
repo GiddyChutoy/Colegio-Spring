307740618fd3a9f8a7c1da85495323e370bdc566
Adrian Mateos <adrian@adrian>
2021-02-23 09:23:56 +0100

    [ADD] COntrolador de combos hecho

--- src/main/java/com/adri/colegio/controllers/rest/ComboRestController.java
+++ src/main/java/com/adri/colegio/controllers/rest/ComboRestController.java
@@ -3,8 +3,15 @@
  */
 package com.adri.colegio.controllers.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.adri.colegio.dao.CombosDAO;
+import com.adri.colegio.dtos.Combo;
 
 /**
  * @author 201907
@@ -13,5 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("v1")
 public class ComboRestController {
-
+	
+	@Autowired
+	CombosDAO combo;
+	
+	@GetMapping(value = "combos/municipios")
+	public List<Combo> comboMunicipios(){
+		List<Combo> listaMunicipios = combo.comboMunicipios();
+		return listaMunicipios;
+	}
+	
+	@GetMapping(value = "combos/alumnos")
+	public List<Combo> comboAlumnos(){
+		List<Combo> listaAlulmnos = combo.comboAlumnos();
+		return listaAlulmnos;
+	}
+	
+	@GetMapping(value = "combos/asignaturas")
+	public List<Combo> comboAsignaturas(){
+		List<Combo> listaAsignaturas = combo.comboAsignaturas();
+		return listaAsignaturas;
+	}
 }
